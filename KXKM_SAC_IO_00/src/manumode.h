79152a46d453cd90f57246f228b29390c720799c
RIRI <ririfonfon10@gmail.com>
2020-02-19 19:26:37 +0100

    Update manumode.h

--- KXKM_SAC_IO_00/src/manumode.h
+++ KXKM_SAC_IO_00/src/manumode.h
@@ -1,9 +1,8 @@
 #define NUMBER_OF_MEM  11
-#define COEF_PREVUE 5
 int MEM[NUMBER_OF_MEM][LULU_PATCHSIZE] = {
   {0      , 0  , 0  , 0  , 0  , 0      , 0        , 0       , 0       , 0         , 0      , 0      , 0      , 0      , 0         , 0          , 0    , 0   , 0   },  // BLACK
   {0      , 0  , 0  , 0  , 0  , 0      , 0        , 0       , 0       , 0         , 0      , 0      , 0      , 0      , 0         , 0          , 0    , 0   , 0   },  // external
-  {255    , 255, 255, 255, 255, 0      , 0        , 0       , 0       , 0         , 90     , 90     , 90     , 90     , 0         , 0          , 255  , 0   , 0   },  // str white
+  {255    , 0  , 0  , 0  , 255, 0      , 0        , 0       , 0       , 0         , 90     , 90     , 90     , 90     , 0         , 0          , 255  , 0   , 0   },  // white
   {255    , 255, 0  , 0  , 0  , 0      , 0        , 0       , 0       , 0         , 90     , 0      , 0      , 0      , 0         , 0          , 255  , 0   , 0   },  // Red
   {255    , 0  , 255, 0  , 0  , 0      , 0        , 0       , 0       , 0         , 0      , 90     , 0      , 0      , 0         , 0          , 255  , 0   , 0   },  // Green
   {255    , 0  , 0  , 255, 0  , 0      , 0        , 0       , 0       , 0         , 0      , 0      , 90     , 0      , 0         , 0          , 255  , 0   , 0   },  // Blue
@@ -14,7 +13,20 @@ int MEM[NUMBER_OF_MEM][LULU_PATCHSIZE] = {
   {255    , 255, 255, 255, 255, 0      , 0        , 0       , 11      , 80        , 110    , 110    , 110    , 110    , 0         , 0          , 255  , 0   , 0   }   // str white
 };
 //{master , r  , g  , b  , w  ,pix mod , pix long , pix_pos , str_mod , str_speed , r_fond , g_fond , b_fond , w_fond , color_mod , mirror_mod , zoom , pw1 , pw2 }
-
+int MEM_PREV[NUMBER_OF_MEM][24] = {
+  { 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0 }, // BLACK
+  { 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0 }, // external
+  { 0   , 0   , 0   , 30  , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0 }, // white
+  { 30  , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0 }, // Red
+  { 0   , 30  , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0 }, // Green
+  { 0   , 0   , 30  , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0 }, // Blue
+  { 30  , 30  , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0 }, // Yellow
+  { 0   , 30  , 30  , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0 }, // Cyan
+  { 30  , 0   , 30  , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 0 }, // Magenta
+  { 30  , 0   , 0   , 0   , 30  , 30  , 0   , 0   , 0   , 30  , 0   , 0   , 0   , 30  , 30  , 0   , 0   , 0   , 30  , 0   , 30  , 0   , 30  , 0 }, // Arc
+  { 0   , 0   , 0   , 50  , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 50  , 0   , 0   , 0   , 0   , 0   , 0   , 0   , 50  , 0   , 0   , 0   , 0 }  // str white
+};
+//{ r1  , g1  , b1  , w1  , r2  , g2  , b2  , w2  , r3  , g3  , w3  , w3  , r4  , g4  , b4  , w4  , r5  , g5  , b5  , w5  , r6  , g6  , b6  , w6  }                      ,             
 void manu_frame(int mem) {
   
   mem = mem % NUMBER_OF_MEM;
@@ -23,14 +35,12 @@ void manu_frame(int mem) {
 
   const int frameSize = adr + LULU_PATCHSIZE;
   uint8_t fakeframe[frameSize];
+int p=0;
+  for (int i = NUM_LEDS_PER_STRIP_max + 9 ; i < NUM_LEDS_PER_STRIP_max + 15 ; i++) {
+        strands[1]->pixels[i] = pixelFromRGBW((MEM_PREV[mem][p]),(MEM_PREV[mem][p+1]),(MEM_PREV[mem][p+2]),(MEM_PREV[mem][p+3]));
+        p = p + 4 ;
+      }
 
-  for (int i = NUM_LEDS_PER_STRIP_max + 9 ; i < NUM_LEDS_PER_STRIP_max + 12 ; i++) {
-        strands[1]->pixels[i] = pixelFromRGBW((MEM[mem][1]/COEF_PREVUE),(MEM[mem][2]/COEF_PREVUE),(MEM[mem][3]/COEF_PREVUE),(MEM[mem][4]/COEF_PREVUE));
-      }
-  for (int i = NUM_LEDS_PER_STRIP_max + 12 ; i < NUM_LEDS_PER_STRIP_max + 15 ; i++) {
-        strands[1]->pixels[i] = pixelFromRGBW((MEM[mem][10]/COEF_PREVUE),(MEM[mem][11]/COEF_PREVUE),(MEM[mem][12]/COEF_PREVUE),(MEM[mem][13]/COEF_PREVUE));
-      }
-  
   for (int i = 0 ; i < LULU_PATCHSIZE; i++) fakeframe[adr + i - 1] = MEM[mem][i];
   onDmxFrame(LULU_uni, adr + LULU_PATCHSIZE, 0, fakeframe);
 }
