fd4f3ec560ae324226dec0f86867279d49d5848f
RIRI <ririfonfon10@gmail.com>
2020-03-02 17:58:10 +0100

    add pin out ATOM

--- KXKM_SAC_IO_00/src/PWM.h
+++ KXKM_SAC_IO_00/src/PWM.h
@@ -1,27 +1,38 @@
 int freq = 40000;
 int resol = 8;
 
-void ledSetup() {
+void ledSetup()
+{
   //// Setup LED for Testing
   ledcSetup(1, freq, resol);
   ledcSetup(2, freq, resol);
+#ifdef K32_SET_HWREVISION
+#if K32_SET_HWREVISION == 2
   ledcAttachPin(17, 1);
   ledcAttachPin(16, 2);
+#else if K32_SET_HWREVISION == 3
+  ledcAttachPin(23, 1);
+  ledcAttachPin(33, 2);
+#endif
+#endif
+
   pinMode(4, OUTPUT);
   digitalWrite(4, HIGH);
 }
 
-
-void channelOne() {
+void channelOne()
+{
   ledcWrite(1, ledChannelOne);
 }
 
-void channelTwo() {
+void channelTwo()
+{
   ledcWrite(2, ledChannelTwo);
 }
 
 //// Setup PWM for LED(s)
-void leds() {
+void leds()
+{
   channelOne();
   channelTwo();
 }
