25a599a648088f8013ff0277ca487c3145d952b6
richard fontaine <ririfonfon10@gmail.com>
2020-03-29 20:10:07 +0200

    Update onDmxFrame.h

--- KXKM_SAC_IO_00/src/onDmxFrame.h
+++ KXKM_SAC_IO_00/src/onDmxFrame.h
@@ -243,5 +243,5 @@ void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t *d
 
 void onArtNetFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t *data)
 {
-    onDmxFrame(universe, length, sequence, data); 
+  onDmxFrame(universe, length, sequence, data);
 }
