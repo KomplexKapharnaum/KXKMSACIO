1cec7a5eaad37ea0e2a5b3cab5e801964c4fb580
maigre <thomas.bohl@gmail.com>
2020-04-14 14:54:15 +0200

    rm config

--- KXKM_SAC_IO_00/src/main.cpp
+++ KXKM_SAC_IO_00/src/main.cpp
@@ -4,12 +4,12 @@
 
 /////////////////////////////////////////ID/////////////////////////////////////////
 
-#define K32_SET_NODEID 81       // board unique id
+// #define K32_SET_NODEID 81       // board unique id
 
-#define RUBAN_TYPE LED_SK6812W_V1    // LED_WS2812_V1  LED_WS2812B_V1  LED_WS2812B_V2  LED_WS2812B_V3  LED_WS2813_V1  LED_WS2813_V2   LED_WS2813_V3  LED_WS2813_V4  LED_SK6812_V1  LED_SK6812W_V1,
-#define LULU_ID 1                 // permet de calculer l'adresse DMX
-#define LULU_TYPE 1               // 1="Sac" 2="Barre" 3="Pince" 4="Fluo" 5="Flex" 6="H&S" 7="Phone" 8="Atom"
-#define LULU_UNI 0                // Univers DM
+// #define RUBAN_TYPE LED_WS2812_V1    // LED_WS2812_V1  LED_WS2812B_V1  LED_WS2812B_V2  LED_WS2812B_V3  LED_WS2813_V1  LED_WS2813_V2   LED_WS2813_V3  LED_WS2813_V4  LED_SK6812_V1  LED_SK6812W_V1,
+// #define LULU_ID 1                 // permet de calculer l'adresse DMX
+// #define LULU_TYPE 1               // 1="Sac" 2="Barre" 3="Pince" 4="Fluo" 5="Flex" 6="H&S" 7="Phone" 8="Atom"
+// #define LULU_UNI 0                // Univers DM
 
 /////////////////////////////////////////Debug///////////////////////////////////////
 
