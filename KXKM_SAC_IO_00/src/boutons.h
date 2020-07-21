77dcede736f4271aa333ae3ad460e756a946cd2d
RIRI <ririfonfon10@gmail.com>
2020-07-21 13:50:21 +0200

    mem h&s

--- KXKM_SAC_IO_00/src/boutons.h
+++ KXKM_SAC_IO_00/src/boutons.h
@@ -77,28 +77,28 @@ void boutons_loop()
 
         // add modulators
         //
-        if (activeMacro == 1)
+        if (activeMacro == 2)
         {
             manu->mod(new K32_mod_sinus)->at(0)->period(8500)->mini(38)->maxi(217);
         }
-        else if (activeMacro == 2)
+        else if (activeMacro == 3)
         {
             manu->mod(new K32_mod_sinus)->at(0)->period(4800)->mini(38)->maxi(217);
         }
-        else if (activeMacro == 8)
-        {
-            manu->mod(new K32_mod_sawtooth)->at(9)->mini(229)->maxi(238)->period(3700)
-        }
         else if (activeMacro == 9)
         {
-            manu->mod(new K32_mod_sawtooth)->at(7)->mini(1)->maxi(255)->period(1000)
+            manu->mod(new K32_mod_sawtooth)->at(9)->mini(229)->maxi(238)->period(3700);
         }
         else if (activeMacro == 10)
         {
-            manu->mod(new K32_mod_pulse)->at(8)->maxi(MEM[activeMacro][8])->param(1, 66)->period(7000);
-            manu->mod(new K32_mod_pulse)->at(1)->at(2)->param(1, 66)->period(7000);
+            manu->mod(new K32_mod_sawtooth)->at(7)->period(7000);
         }
-        if (activeMacro == 13)
+        else if (activeMacro == 11)
+        {
+            manu->mod(new K32_mod_pulse)->at(1)->at(2)->at(4)->param(1, 66)->period(7000);
+            manu->mod(new K32_mod_pulse)->at(8)              ->param(1, 66)->period(7000);
+        }
+        else if (activeMacro == 14)
         {
             manu->mod(new K32_mod_sinus)->at(0)->period(8500)->mini(38)->maxi(217);
         }
