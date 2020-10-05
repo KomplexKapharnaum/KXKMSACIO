570e5dce91782a201349c77fc470eb4ade63316b
RIRI <ririfonfon10@gmail.com>
2020-10-05 16:02:46 +0200

    Create test.h

---
+++ KXKM_ESP_DMX _4_PWM/src/test.h
@@ -0,0 +1,47 @@
+
+
+
+
+void light_tests() {
+
+  int timeStep = 800;
+  
+  // INIT TEST
+    k32->light->anim( 0, "test0",   new K32_anim_test )->push(timeStep)->master(MASTER_PREV)->play();
+    k32->light->anim( 1, "test1",   new K32_anim_test )->push(timeStep)->master(MASTER_PREV)->play();
+
+  // PWM TEST
+    k32->pwm->set(0, 255);
+    k32->pwm->set(1, 0);
+    k32->pwm->set(2, 255);
+    k32->pwm->set(3, 0);    
+    delay(timeStep);
+    k32->pwm->set(0, 0);
+    k32->pwm->set(1, 255);
+    k32->pwm->set(2, 0);
+    k32->pwm->set(3, 255); 
+    delay(timeStep);
+    k32->pwm->set(0, 127);
+    k32->pwm->set(1, 127);
+    k32->pwm->set(2, 127);
+    k32->pwm->set(3, 127); 
+    delay(timeStep);
+    k32->pwm->set(0, 0);
+    k32->pwm->set(1, 0);
+    k32->pwm->set(2, 0);
+    k32->pwm->set(3, 0); 
+
+  // WAIT END
+    k32->light->anim("test0")->wait();
+  
+  // COLOR TEST
+    // k32->light->anim(0, "colorA", new K32_anim_color, 10, 0)->push(50, 255, 200, 0, 0)->play(1000)->wait();
+
+  // DMX TEST
+    // K32_anim* artnet = k32->light->anim("artnet");
+    // artnet->push(MEM[8], LULU_PATCHSIZE)->push(100);
+    // artnet->mod("saw",    new K32_mod_sinus)  ->at(0)->play();
+    // artnet->mod("triangle", new K32_mod_triangle)   ->at(15)->period(10000)->play();
+    // artnet->mod("fadeout",  new K32_mod_fadeout)    ->at(0)->period(1000)->play();
+    
+}
