807d885a90af539aad1874ffec905ea6ad54d308
RIRI <ririfonfon10@gmail.com>
2020-10-23 18:50:35 +0200

    Update mem_cube_parled.h

--- KXKM_ESP_DMX _4_PWM/src/macro/mem_cube_parled.h
+++ KXKM_ESP_DMX _4_PWM/src/macro/mem_cube_parled.h
@@ -5,7 +5,7 @@
 #define NUMBER_OF_MEM 16 // stm leave in last mem
 #endif
 
-// 
+//
 // MEM ANIMATOR DATA ! modulateur relatif a la valeur du tableau
 //
 uint8_t MEM[NUMBER_OF_MEM][LULU_PATCHSIZE] = {};
@@ -13,49 +13,49 @@ uint8_t MEM_NO_WIFI[LULU_PATCHSIZE] = {};
 
 void init_mem()
 {
-  for (int n = 0 ; n < NUMBER_OF_MEM ; n++)
-   {
-      for (int i = 0 ; i < LULU_PATCHSIZE -4 ; i++) 
-      {
-           MEM[n][i] = MEM_PARLED[n][i];
-           MEM_NO_WIFI[i] = MEM_PARLED_NO_WIFI[i];
-      }
-      for (int i = LULU_PATCHSIZE -4 ; i < LULU_PATCHSIZE  ; i++) 
-      {
-           MEM[n][i]=MEM_PWM[n][i - LULU_PATCHSIZE - 4];
-           MEM_NO_WIFI[i] = MEM_PWM_NO_WIFI[i -4];
-      }
-   }
+    for (int n = 0; n < NUMBER_OF_MEM; n++)
+    {
+        for (int i = 0; i < LULU_PATCHSIZE - 4; i++)
+        {
+            MEM[n][i] = MEM_PARLED[n][i];
+            MEM_NO_WIFI[i] = MEM_PARLED_NO_WIFI[i];
+        }
+        for (int i = LULU_PATCHSIZE - 4; i < LULU_PATCHSIZE; i++)
+        {
+            MEM[n][i] = MEM_PWM[n][i - LULU_PATCHSIZE - 4];
+            MEM_NO_WIFI[i] = MEM_PWM_NO_WIFI[i - 4];
+        }
+    }
 }
 
-// 
+//
 // PREVIEW PIXEL MAP
 //
-uint8_t MEM_PREV[NUMBER_OF_MEM][LULU_PREVPIX*4] = {
-    {1, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0},    // 00 red
-    {0, 1, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0},    // 01 Green
-    {0, 0, 1, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0},    // 02 Blue
-    {0, 0, 0, 1,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0},    // 03 White
-    {1, 0, 0, 0,  0, 1, 0, 0,  0, 0, 1, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0},    // 04 rvb **3 4 5**
-    {0, 0, 0, 1,  0, 0, 0, 1,  0, 0, 0, 1,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0},    // 05 str rnd 67
-    {0, 0, 0, 1,  0, 0, 0, 1,  0, 0, 0, 1,  0, 0, 0, 1,  0, 0, 0, 0,  0, 0, 0, 0},    // 06 str all 67
-    {0, 0, 0, 1,  0, 0, 0, 1,  0, 0, 0, 1,  0, 0, 0, 1,  0, 0, 0, 1,  0, 0, 0, 0},    // 07 str all 42
-    {0, 0, 0, 0,  0, 0, 0, 1,  0, 0, 0, 1,  0, 0, 0, 0,  0, 0, 0, 1,  0, 0, 0, 1},    // 08 str all lent
-    {0, 0, 0, 1,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 1},    // 09 circule w **9** 20>255
-    {0, 0, 0, 1,  0, 0, 0, 1,  0, 0, 1, 0,  0, 0, 1, 0,  0, 0, 0, 0,  0, 0, 0, 0},    // 10 circus w b
-    {0, 0, 1, 0,  0, 0, 1, 0,  0, 0, 1, 0,  0, 0, 0, 1,  0, 0, 0, 1,  0, 0, 0, 1},    // 11 str w -> blue
-    {0, 0, 1, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0},    // 12 blue all
-    {0, 0, 1, 0,  0, 0, 0, 0,  0, 0, 1, 0,  0, 0, 0, 0,  0, 0, 1, 0,  0, 0, 0, 0},    // 13 blue parcielle
-    {0, 0, 0, 0,  0, 0, 1, 0,  0, 0, 0, 0,  0, 0, 1, 0,  0, 0, 0, 0,  0, 0, 0, 0},    // 14 respi blue 
-    {0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0},    // 15 BLACK stm leave last mem
+uint8_t MEM_PREV[NUMBER_OF_MEM][LULU_PREVPIX * 4] = {
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // 00 red
+    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // 01 Green
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // 02 Blue
+    {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // 03 White
+    {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // 04 rvb **3 4 5**
+    {0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // 05 str rnd 67
+    {0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, // 06 str all 67
+    {0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0}, // 07 str all 42
+    {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1}, // 08 str all lent
+    {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 09 circule w **9** 20>255
+    {0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // 10 circus w b
+    {0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1}, // 11 str w -> blue
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // 12 blue all
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, // 13 blue parcielle
+    {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // 14 respi blue
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // 15 BLACK stm leave last mem
 };
 //  {r1,g1,b1,w1, r2,g2,b2,w2, r3,g3,w3,w3, r4,g4,b4,w4, r5,g5,b5,w5, r6,g6,b6,w6},
 
-
-// 
+//
 // APPLY MACRO WITH CUSTOM MODULATORS INTO anim
 //
-void load_mem(K32_anim *anim, int macro) {
+void load_mem(K32_anim *anim, int macro)
+{
 
     // remove disposable modulators
     //
@@ -73,7 +73,7 @@ void load_mem(K32_anim *anim, int macro) {
         anim->mod(new K32_mod_sinus)->at(1)->period(8500)->phase(90)->mini(0)->maxi(255);
         anim->mod(new K32_mod_sinus)->at(2)->period(8500)->phase(180)->mini(0)->maxi(255);
     }
-     else if (macro == 6)
+    else if (macro == 6)
     {
         anim->mod(new K32_mod_pulse)->at(1)->at(2)->at(3)->at(7)->at(8)->at(6)->at(5)->param(1, 10)->period(70);
     }
@@ -100,7 +100,5 @@ void load_mem(K32_anim *anim, int macro) {
         anim->mod(new K32_mod_sinus)->at(0)->at(1)->at(2)->period(8500)->mini(38)->maxi(217);
     }
 }
-
-
 
 #endif
