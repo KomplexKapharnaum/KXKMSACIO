0c6e01cb77362c2cede616d132a5e7a9c0ed3916
richard fontaine <ririfonfon10@gmail.com>
2020-10-26 10:04:24 +0100

    add pince

--- KXKM_ESP_DMX _4_PWM/src/settings.h
+++ KXKM_ESP_DMX _4_PWM/src/settings.h
@@ -22,7 +22,7 @@
     #define LULU_PATCHSIZE 18 // Taille du patch DMX pour cet Fixture ** 18 = sk_pw / 16 = strobe led / 5 = par led / 20 = cube strobe dmx / 9 = cube par led dmx
     #define LULU_UNI 1   // Univers DMX   0=SAC 1=Barre 2=Chariot 3=Dmx 4=Cube_strobe 5=Cube_parled 6=Sucette 7=minikolor
     #define FAKE_CURRENT 0
-    // #include "macro/Type/SK/mem.h" 
+    // #include "macro/Type/SK/mem.h" // mem global sk & pwm
 
     #define ON_NO_WIFI 0
     #include "macro/Type/4pwm/mem_4pwm.h"  // defo
@@ -36,7 +36,12 @@
     #define LULU_PATCHSIZE 18 // 18 = sk_pw / 16 = strobe led / 5 = par led / 20 = cube strobe dmx / 9 = cube par led dmx / 13 = cube minikolor
     #define LULU_UNI 7   // Univers DMX   0=SAC 1=Barre 2=Chariot 3=Dmx 4=Cube_strobe 5=Cube_parled 6=Sucette 7=minikolor
     #define FAKE_CURRENT 0
-    #include "macro/Type/SK/mem.h" 
+    // #include "macro/Type/SK/mem.h" // mem global sk & pwm
+
+    #define ON_NO_WIFI 0
+    #include "macro/Type/4pwm/mem_4pwm.h"  // defo
+    #include "macro/Type/SK/mem_sk.h"  // defo
+    #include "macro/mem_pince_pwm.h"
 
   #elif LULU_TYPE == 4
     #define L_TYPE "Fluo"
