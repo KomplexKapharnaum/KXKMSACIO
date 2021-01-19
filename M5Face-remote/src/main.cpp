39446d61b0ff4d799c7ab8b5fe860d3d68ad1c7d
richard fontaine <ririfonfon10@gmail.com>
2021-01-19 12:04:46 +0100

    add riri dev home

--- M5Face-remote/src/main.cpp
+++ M5Face-remote/src/main.cpp
@@ -1,16 +1,15 @@
 #include <M5Stack.h>
 #include <M5ez.h>
 
-#define KEYBOARD_I2C_ADDR     0X08
-#define KEYBOARD_INT          5
+#define KEYBOARD_I2C_ADDR 0X08
+#define KEYBOARD_INT 5
 
 #include <K32.h> // https://github.com/KomplexKapharnaum/K32-lib
 K32 *k32;
 
-
 void setup()
 {
-  #include <themes/dark.h>
+#include <themes/dark.h>
   ez.begin();
   M5.Power.begin();
   M5.Speaker.mute();
@@ -31,19 +30,20 @@ void setup()
 
   /////////////////////////////////////////////// WIFI //////////////////////////////////////
   k32->init_wifi("Rasta-Remote");
+  k32->wifi->staticIP("2.0.0.11" , "2.0.0.1", "255.0.0.0");
   // k32->wifi->connect("kxkm24", NULL); //KXKM
-  k32->wifi->connect("mgr4g", NULL); //MGR
+  // k32->wifi->connect("mgr4g", NULL); //MGR
+  k32->wifi->connect("riri_new", "B2az41opbn6397"); //Riri dev home
 
   /////////////////////////////////////// MQTT //////////////////////////////////////
   k32->init_mqtt({
-      // .broker = "10.0.0.1",
-      .broker = "192.168.43.100",
+      // .broker = "2.0.0.1",// Komplex
+      .broker = "2.0.0.10", // Riri dev home
+      // .broker = "192.168.43.100",//MGR
       .beatInterval = 0,  // heartbeat interval milliseconds (0 = disable)
       .beaconInterval = 0 // full beacon interval milliseconds (0 = disable)
   });
-
 }
-
 
 void loop()
 {
@@ -51,47 +51,48 @@ void loop()
 
   // A/B/C Buttons
   //
-  if (M5.BtnA.wasPressed() || M5.BtnA.isPressed()) {
-      
+  if (M5.BtnA.wasPressed() || M5.BtnA.isPressed())
+  {
   };
 
-  if (M5.BtnC.wasPressed() || M5.BtnC.isPressed()) {
-      
+  if (M5.BtnC.wasPressed() || M5.BtnC.isPressed())
+  {
   };
 
-  if (M5.BtnB.wasPressed()) {
-      
+  if (M5.BtnB.wasPressed())
+  {
   };
-
 
   // FACE
   //
-  if (digitalRead(KEYBOARD_INT) == LOW) {
-    Wire.requestFrom(KEYBOARD_I2C_ADDR, 1);  // request 1 byte from keyboard
-    while (Wire.available()) {
-      uint8_t key_val = Wire.read();                  // receive a byte as character
+  if (digitalRead(KEYBOARD_INT) == LOW)
+  {
+    Wire.requestFrom(KEYBOARD_I2C_ADDR, 1); // request 1 byte from keyboard
+    while (Wire.available())
+    {
+      uint8_t key_val = Wire.read(); // receive a byte as character
       String msg((char)key_val);
 
-      switch ((char)key_val) 
+      switch ((char)key_val)
       {
-        case '0': 
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-        case '9':
-          k32->mqtt->publish("k32/all/leds/mem", msg.c_str(), 1); 
-          msg += ": k32/all/leds/mem "+msg;
-          break;
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        k32->mqtt->publish("k32/all/leds/mem", msg.c_str(), 1);
+        msg += ": k32/all/leds/mem " + msg;
+        break;
 
-        case '.': 
-          k32->mqtt->publish("k32/all/leds/stop", nullptr, 1); 
-          msg += ": k32/all/leds/stop";
-          break;
+      case '.':
+        k32->mqtt->publish("k32/all/leds/stop", nullptr, 1);
+        msg += ": k32/all/leds/stop";
+        break;
         // case '=': k32->mqtt->publish("/event/KEY_KPENTER-down"); break;
         // case '-': k32->mqtt->publish("/event/KEY_KPMINUS-down"); break;
         // case '+': k32->mqtt->publish("/event/KEY_KPPLUS-down"); break;
@@ -105,6 +106,4 @@ void loop()
       ez.msgBox("Rasta Remote", msg, "", false);
     }
   }
-
 }
-
