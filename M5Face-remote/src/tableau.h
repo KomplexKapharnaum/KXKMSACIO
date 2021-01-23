269f18b203bad40c8e7208603cbadd7b8de78dd3
richard fontaine <ririfonfon10@gmail.com>
2021-01-24 00:28:45 +0100

    Create tableau.h

---
+++ M5Face-remote/src/tableau.h
@@ -0,0 +1,14 @@
+#define max_clients 10
+
+uint8_t clients = 1;
+String T_ID[max_clients];  // id
+String T_CHA[max_clients]; // channel
+String T_VER[max_clients]; // version
+String T_MAC[max_clients]; // mac adress
+String T_IP[max_clients];  // ip
+String T_WIF[max_clients]; // wifi rssi
+String T_CON[max_clients]; // ?????????
+String T_BAT[max_clients]; // battery
+String T_FAL[max_clients]; // ?????????
+String T_RUN[max_clients]; // playeur state
+String T_END[max_clients]; // message end , utile ?
