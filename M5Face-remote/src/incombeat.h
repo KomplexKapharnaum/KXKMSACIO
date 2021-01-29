8a7c2f16f56c6e9a333169af8459d2b341c30b34
richard fontaine <ririfonfon10@gmail.com>
2021-01-29 13:44:14 +0100

    correct range to T_inlife

--- M5Face-remote/src/incombeat.h
+++ M5Face-remote/src/incombeat.h
@@ -13,31 +13,31 @@ void incombeat(char *payload, size_t length)
 
     for (int k = 0; k < max_clients; k++)
     {
-        if (T_inlife[k][1] == beatresult)
+        if (T_inlife[k][0] == beatresult)
         {
             LOGINL(" == ");
-            previousTime = T_inlife[k][2];
+            previousTime = T_inlife[k][1];
             if ((currentTime - previousTime) < 2500)
             {
                 LOGINL(" < 2500 ");
-                T_inlife[k][2] = currentTime;
-                T_inlife[k][3] = 1;
+                T_inlife[k][1] = currentTime;
+                T_inlife[k][2] = 1;
                 break;
             }
             else
             {
                 LOGINL("ELSE  NOT < 2500 ");
-                T_inlife[k][2] = currentTime;
-                T_inlife[k][3] = 0;
+                T_inlife[k][1] = currentTime;
+                T_inlife[k][2] = 0;
                 break;
             }
         }
-        else if (T_inlife[k][3] != 1)
+        else if (T_inlife[k][2] != 1)
         {
             LOGINL(" != 1");
-            T_inlife[k][1] = beatresult;
-            T_inlife[k][2] = currentTime;
-            T_inlife[k][3] = 1;
+            T_inlife[k][0] = beatresult;
+            T_inlife[k][1] = currentTime;
+            T_inlife[k][2] = 1;
             break;
         }
     }
