8dfed6e83ab64bc6dab472e3bd051423975ae3be
richard fontaine <ririfonfon10@gmail.com>
2021-01-30 14:12:16 +0100

    add master value

--- M5Face-remote/src/menu.h
+++ M5Face-remote/src/menu.h
@@ -2,10 +2,96 @@
 void remote_mqtt();
 void powerOff();
 void mainmenu_mon_id();
-void mainmenu_monitor();
+void menu_monitor();
 void mainmenu();
+void master_value();
 
 int16_t who_result;
+
+void master_value()
+{
+    bool equal = false;
+    bool bad = false;
+    String res_value = "";
+    String msg = "";
+    int res;
+
+    msg += " Enter value to master 0 -> 255 & =";
+    ez.msgBox("M5 REMOTE", msg, "#####", false);
+
+    while (equal != true)
+    {
+        res = atoi(res_value.c_str());
+        // FACE
+        //
+        if (digitalRead(KEYBOARD_INT) == LOW)
+        {
+            Wire.requestFrom(KEYBOARD_I2C_ADDR, 1); // request 1 byte from keyboard
+            while (Wire.available())
+            {
+                uint8_t key_val = Wire.read(); // receive a byte as character
+                String value((char)key_val);
+
+                switch ((char)key_val)
+                {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    if (bad == true)
+                        bad = false;
+                    res_value += value;
+                    break;
+
+                case '.':
+                    break;
+                case '=':
+
+                    if (res < 256)
+                    {
+                        k32->mqtt->publish("k32/all/leds/master", (res_value).c_str(), 1);
+                        equal = true;
+                    }
+                    else
+                    {
+                        bad = true;
+                        res_value = "";
+                        msg = "";
+                        msg += " Enter value to master 0 -> 255 & =";
+                        ez.msgBox("M5 REMOTE", msg, "#####", false);
+                    }
+                    break;
+                case '-':
+
+                    break;
+                case '+':
+
+                    break;
+
+                case 'A':
+                    res_value = "";
+
+                    break;
+                case 'M':
+
+                    break;
+                case '%':
+
+                    break;
+                }
+                LOG(value);
+                if (bad != true)
+                    ez.msgBox("M5 REMOTE", res_value, "#####", false);
+            }
+        }
+    }
+}
 
 void remote_mqtt()
 {
@@ -101,8 +187,8 @@ void remote_mqtt()
                 case '=':
                     if (fonction == 0)
                     {
-                        k32->mqtt->publish("k32/all/leds/master/fadeout", nullptr, 1);
-                        msg += " k32/all/leds/master/fadeout";
+                        master_value();
+                        msg = "Master SEND";
                     }
                     else if (fonction == 1)
                     {
@@ -137,7 +223,7 @@ void remote_mqtt()
                     if (fonction == 0)
                     {
                         k32->mqtt->publish("k32/all/leds/master/full", nullptr, 1);
-                        msg += " k32/all/leds/mem/full";
+                        msg += " k32/all/leds/master/full";
                     }
                     else if (fonction == 1)
                     {
@@ -147,7 +233,7 @@ void remote_mqtt()
                     if (fonction == 0)
                     {
                         k32->mqtt->publish("k32/all/leds/master/fadein", nullptr, 1);
-                        msg += " k32/all/leds/mem/fadein";
+                        msg += " k32/all/leds/master/fadein";
                     }
                     else if (fonction == 1)
                     {
@@ -157,7 +243,7 @@ void remote_mqtt()
                     if (fonction == 0)
                     {
                         k32->mqtt->publish("k32/all/leds/master/fadeout", nullptr, 1);
-                        msg += " k32/all/leds/mem/fadeout";
+                        msg += " k32/all/leds/master/fadeout";
                     }
                     else if (fonction == 1)
                     {
@@ -200,23 +286,23 @@ void mainmenu_mon_id()
     mainmenu_mon_id.run();
 }
 
-void mainmenu_monitor()
+void menu_monitor()
 {
     who_result = 0;
     String on_off_line;
 
     if (clients == -1)
     {
-        ezMenu mainmenu_monitor("NO ESP");
+        ezMenu menu_monitor("NO ESP");
         ez.msgBox("NO ESP", "NO ESP FOUND", "#Back####", true);
-        mainmenu_monitor.run();
+        menu_monitor.run();
     }
     else
     {
-        ezMenu mainmenu_monitor("ESP find");
-        mainmenu_monitor.upOnFirst("last|up");
-        mainmenu_monitor.downOnLast("first|down");
-        mainmenu_monitor.buttons("up#Back#select##down#");
+        ezMenu menu_monitor("ESP find");
+        menu_monitor.upOnFirst("last|up");
+        menu_monitor.downOnLast("first|down");
+        menu_monitor.buttons("up#Back#select##down#");
 
         for (int k = 0; k <= clients; k++)
         {
@@ -236,21 +322,19 @@ void mainmenu_monitor()
                         on_off_line = " ... .. .";
                         T_inlife[j][2] = 0;
                     }
-                    
                 }
-            
             }
 
-            mainmenu_monitor.addItem(String(k) + " ESP : " + String(T_ID[k]) + on_off_line);
+            menu_monitor.addItem(String(k) + " ESP : " + String(T_ID[k]) + on_off_line);
         }
 
-        while (mainmenu_monitor.runOnce())
+        while (menu_monitor.runOnce())
         {
-            who_result = mainmenu_monitor.pick();
+            who_result = menu_monitor.pick();
             if (who_result > 0)
                 mainmenu_mon_id();
         }
-        mainmenu_monitor.run();
+        menu_monitor.run();
     }
 }
 
@@ -259,7 +343,7 @@ void mainmenu()
     ezMenu mainmenu("Welcome to M5");
     mainmenu.txtBig();
     mainmenu.addItem("REMOTE", remote_mqtt);
-    mainmenu.addItem("MONITOR", mainmenu_monitor);
+    mainmenu.addItem("MONITOR", menu_monitor);
     mainmenu.addItem("SET UP", ez.settings.menu);
     mainmenu.addItem("POWER OFF", powerOff);
     mainmenu.upOnFirst("last|up");
