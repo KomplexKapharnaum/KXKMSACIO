0e35fe98a3a64667a95ff101ad74ed29147d56ba
richard fontaine <ririfonfon10@gmail.com>
2021-02-03 23:54:33 +0100

    remove log

--- M5Face-remote/src/incombeat.h
+++ M5Face-remote/src/incombeat.h
@@ -13,18 +13,15 @@ void incombeat(char *payload, size_t length)
     {
         if (T_inlife[k][0] == beatresult)
         {
-            LOGINL(" == ");
             previousTime = T_inlife[k][1];
             if ((currentTime - previousTime) < 2500)
             {
-                LOGINL(" < 2500 ");
                 T_inlife[k][1] = currentTime;
                 T_inlife[k][2] = 1;
                 break;
             }
             else
             {
-                LOGINL("ELSE  NOT < 2500 ");
                 T_inlife[k][1] = currentTime;
                 T_inlife[k][2] = 0;
                 break;
@@ -32,7 +29,6 @@ void incombeat(char *payload, size_t length)
         }
         else if (T_inlife[k][2] != 1)
         {
-            LOGINL(" != 1");
             T_inlife[k][0] = beatresult;
             T_inlife[k][1] = currentTime;
             T_inlife[k][2] = 1;
