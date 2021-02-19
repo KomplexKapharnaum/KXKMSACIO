8b740929cbefdf2e82573ee672c7485781e1da94
richard fontaine <ririfonfon10@gmail.com>
2021-02-19 14:41:36 +0100

    preset_light

--- M5Face-remote/src/preset_light.h
+++ M5Face-remote/src/preset_light.h
@@ -1,11 +1,11 @@
-#ifndef mem_color_h
-#define mem_color_h
+#ifndef preset_light_h
+#define preset_light_h
 
 #ifndef number_mem
 #define number_mem 10
 #endif
 
-uint8_t MEM_COLOR[number_mem][4] = {
+uint8_t PRESET_COLOR[number_mem][4] = {
     {023, 023, 023, 023}, // 00 DIAL ALL
     {255, 000, 000, 000}, // 01 RED
     {000, 255, 000, 000}, // 02 GREEN
@@ -19,7 +19,7 @@ uint8_t MEM_COLOR[number_mem][4] = {
 };
 //  {red,gree,blue,whit}
 
-String NAME_MEM_COLOR[number_mem] = {
+String NAME_PRESET_COLOR[number_mem] = {
     " DIA ALL ",  // 0
     " RED ",      // 1
     " GREEN ",    // 2
@@ -30,6 +30,33 @@ String NAME_MEM_COLOR[number_mem] = {
     " CYAN ",     // 7
     " MAGENTA ",  // 8
     " FULL ALL ", // 9
+};
+
+uint8_t PRESET_STR[number_mem] = {
+    5,   // 00 OPEN
+    15,  // 01 STR
+    25,  // 02 SMOOTH
+    31,  // 03 RND 1
+    71,  // 04 RND 32
+    110, // 05 RND 64
+    115, // 06 *BLINK
+    201, // 07 0 RND 1
+    228, // 08 0 RND 32
+    255, // 09 O RND 64
+};
+
+String NAME_PRESET_STR[number_mem] = {
+    " OPEN ",     //00
+    " STR ",      //01
+    " SMOOTH ",   //02
+    " RND 1 ",    //03
+    "RND 32 ",    //04
+    " RND 64 ",   //05
+    " *BLINK* ",  //06
+    " O RND 1",   //07
+    " O RND 32 ", //08
+    " O RND 64 ", //09
+   
 };
 
 #endif
