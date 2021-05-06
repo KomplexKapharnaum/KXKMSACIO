8e30540b18c943f6fbc82948ab1fe09ebff4aa40
richard fontaine <ririfonfon10@gmail.com>
2021-05-06 18:21:52 +0200

    fix pwm addr

--- KXKM_ESP_DMX _4_PWM/src/anim_dmx_strip.h
+++ KXKM_ESP_DMX _4_PWM/src/anim_dmx_strip.h
@@ -190,11 +190,11 @@ class Anim_dmx_strip : public K32_anim {
       //PWM
       if (k32->pwm)
       {
-        k32->pwm->set(0, data[16]*this->master()/255 );
-        k32->pwm->set(1, data[17]*this->master()/255 );
-        k32->pwm->set(2, data[18]*this->master()/255 );
-        k32->pwm->set(3, data[19]*this->master()/255 );
-        // LOGF5("ANIM: -> Red %d PWM %d %d %d %d \n", data[3], data[16], data[17], data[18], data[19]);
+        k32->pwm->set(0, data[LULU_PATCHSIZE-4]*this->master()/255 );
+        k32->pwm->set(1, data[LULU_PATCHSIZE-3]*this->master()/255 );
+        k32->pwm->set(2, data[LULU_PATCHSIZE-2]*this->master()/255 );
+        k32->pwm->set(3, data[LULU_PATCHSIZE-1]*this->master()/255 );
+        // LOGF5("ANIM: -> Red %d PWM %d %d %d %d \n", data[3], data[LULU_PATCHSIZE-4], data[LULU_PATCHSIZE-3], data[LULU_PATCHSIZE-2], data[LULU_PATCHSIZE-1]);
       }
 
       // LOGF("ANIM: %s frame ", name());
