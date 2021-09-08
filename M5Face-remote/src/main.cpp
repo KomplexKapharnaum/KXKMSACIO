43d20b89d63042842a66c9e1e99d1bdee4abb0b8
richard fontaine <ririfonfon10@gmail.com>
2021-09-08 14:55:16 +0200

    add mesh setting

--- M5Face-remote/src/main.cpp
+++ M5Face-remote/src/main.cpp
@@ -60,7 +60,8 @@ void setup()
 
   /////////////////////////////////////////////// WIFI //////////////////////////////////////
   k32->init_wifi("M5-Remote");
-  k32->wifi->staticIP("2.0.0.93", "2.0.0.1", "255.0.0.0");
+  k32->wifi->staticIP("2.0.0.93", "2.0.0.1", "255.0.0.0");//KXKM
+  // k32->wifi->staticIP("10.0.0.93", "10.0.0.1", "255.0.0.0");//KXKM MESH
   k32->wifi->connect("kxkm24", NULL); //KXKM
   // k32->wifi->connect("mgr4g", NULL); //MGR
   // k32->wifi->connect("riri_new", "B2az41opbn6397"); //Riri dev home
@@ -68,6 +69,7 @@ void setup()
   // ez.wifi.add("SSID", "KEY", "IP", "MASK", "GATEWAY","BROKER");
   ez.wifi.add("kxkm24", "", "2.0.0.93", "255.0.0.0", "2.0.0.1", "2.0.0.1");                        //KXKM
   ez.wifi.add("kxkm24lulu", "", "2.0.0.93", "255.0.0.0", "2.0.0.1", "2.0.0.1");                    //KXKM lulu
+  ez.wifi.add("kxkm24", "", "10.0.0.93", "255.0.0.0", "10.0.0.1", "10.0.0.1");                        //KXKM MESH
   ez.wifi.add("kxkm-wifi", "KOMPLEXKAPHARNAUM", "0.0.0.0", "0.0.0.0", "0.0.0.0");                  //KXKM
   ez.wifi.add("mgr4g", "", "0.0.0.0", "0.0.0.0", "0.0.0.0", "0.0.0.0");                            //MGR
   ez.wifi.add("interweb", "superspeed37", "0.0.0.0", "0.0.0.0", "0.0.0.0", "0.0.0.0");             //Maigre dev home
@@ -91,6 +93,7 @@ void setup()
 
   k32->mqtt->start({
       .broker = "2.0.0.1",// Komplex
+      // .broker = "10.0.0.1",// Komplex MESH
       // .broker = "2.0.0.10", // Riri dev home
       // .broker = "192.168.43.100",//MGR
       .beatInterval = 0,  // heartbeat interval milliseconds (0 = disable)
