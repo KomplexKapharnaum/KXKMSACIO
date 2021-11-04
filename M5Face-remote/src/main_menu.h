7907b67fac2cc2e5000a34a85d0040b6695e5fa2
richard fontaine <ririfonfon10@gmail.com>
2021-11-04 09:38:23 +0100

    Update main_menu.h

--- M5Face-remote/src/main_menu.h
+++ M5Face-remote/src/main_menu.h
@@ -3,6 +3,7 @@ void powerOff();
 
 #include "light_menu.h"
 #include "audio_menu.h"
+#include "maree_menu.h"
 #include "monitor_menu.h"
 
 void main_menu()
@@ -11,6 +12,7 @@ void main_menu()
     main_menu.txtBig();
     main_menu.addItem("REMOTE LULU", remote_light);
     main_menu.addItem("REMOTE AUDIO", remote_audio);
+    main_menu.addItem("REMOTE MAREE", remote_maree);
     main_menu.addItem("MONITOR", monitor_menu);
     main_menu.addItem("SETTINGS", ez.settings.menu);
     main_menu.addItem("POWER OFF", powerOff);
