8d3f3a604e4a8b79e4f4cc75058cc3eb1e6557cc
maigre <thomas.bohl@gmail.com>
2021-12-13 23:04:14 +0100

    artnet on ELP

--- KXKM_ESP_DMX_4_PWM/src/type/50-elp_DMX.h
+++ KXKM_ESP_DMX_4_PWM/src/type/50-elp_DMX.h
@@ -6,7 +6,7 @@
 #define DMXOUT_ADDR 20
 #define DMXFIXTURE_PATCHSIZE ELP_PIXELS
 
-#define ARTNET_ENABLE 0
+#define ARTNET_ENABLE 1
 
 #include "macro/Type/SK/mem_sk.h" // defo
 
@@ -42,8 +42,25 @@ void init_lights()
         ->drawTo(elp)
         ->bank(new BankSK)
         ->remote(true)
-        ->push(0)
+        ->push(0);
+
+
+    // ANIM leds - artnet
+    light->anim("artnet-elp", new Anim_dmx_strip, LULU_STRIP_SIZE)
+        ->drawTo(elp)
         ->play();
+
+    // ARTNET: subscribe dmx frame
+    int memSize = light->anim("mem-elp")->bank()->preset_size();
+
+    artnet->onDmx( {
+        .address    = (1 + (light->id() - 1) * memSize), 
+        .framesize  = memSize, 
+        .callback   = [](const uint8_t *data, int length) 
+            { 
+                light->anim("artnet-elp")->push(data, length);
+            }
+        });
 
 
     //
@@ -55,9 +72,31 @@ void init_lights()
     // Bind macro change to Animation mem
     k32->on("remote/macro", [](Orderz *order)
         {
-            light->anim("mem-elp")->mem(order->getData(0)->toInt())->play();
+            light->anim("mem-elp")->mem(order->getData(0)->toInt());
+            remote->setState(REMOTE_MANU); 
             LOGF("mem-elp: %d\n", order->getData(0)->toInt());
         });
+
+    k32->on("remote/state", [](Orderz* order){
+
+        remoteState stateR = (remoteState) order->getData(0)->toInt();
+
+        // AUTO
+        if (stateR == REMOTE_AUTO)
+        {
+            light->anim("mem-elp")->stop();
+            light->anim("artnet-elp")->play();
+            LOG("REMOTE: -> Mode AUTO");
+        }
+
+        // MANU
+        else if (stateR == REMOTE_MANU || stateR == REMOTE_MANU_LAMP || stateR == REMOTE_MANU_STM)
+        {
+            light->anim("artnet-elp")->stop();
+            light->anim("mem-elp")->play();
+            LOG("REMOTE: -> Mode MANU");
+        }
+    });
 
     
 }
