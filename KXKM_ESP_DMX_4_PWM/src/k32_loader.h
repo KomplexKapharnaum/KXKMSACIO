5d39706ca2f97327750f598de4048fa4fd862db8
maigre <thomas@37m.gr>
2022-07-14 00:03:10 +0200

    enable BT and low mem warning

--- KXKM_ESP_DMX_4_PWM/src/k32_loader.h
+++ KXKM_ESP_DMX_4_PWM/src/k32_loader.h
@@ -157,7 +157,7 @@ void k32_setup() {
         // BLUETOOTH
         LOG("NETWORK: bluetooth BROKEN");
         
-        // bt = new K32_bluetooth(k32, "k32-" + String(k32->system->id()));
+        bt = new K32_bluetooth(k32, "k32-" + String(k32->system->id()));
 
         // // TODO use event !
         // bt->onConnect([&]() {
