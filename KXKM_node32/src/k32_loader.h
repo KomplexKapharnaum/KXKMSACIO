7aa81cb21e8a1a14eaf79dd6f679aaa1a65498dd
richard fontaine <ririfonfon10@gmail.com>
2025-03-31 16:52:52 +0200

    dipswitch && dmx in PREMISSE

--- KXKM_node32/src/k32_loader.h
+++ KXKM_node32/src/k32_loader.h
@@ -1,82 +1,88 @@
 
 #include <K32.h> // https://github.com/KomplexKapharnaum/K32-lib
-K32* k32 = nullptr;
+K32 *k32 = nullptr;
 
 #include <hardware/K32_stm32.h>
-K32_stm32* stm32 = nullptr;
+K32_stm32 *stm32 = nullptr;
 
 #include <hardware/K32_buttons.h>
-K32_buttons* buttons = nullptr;
+K32_buttons *buttons = nullptr;
 
 #include <hardware/K32_mcp.h>
-K32_mcp* mcp = nullptr;
+K32_mcp *mcp = nullptr;
 
 #include <hardware/K32_power.h>
-K32_power* power = nullptr;
+K32_power *power = nullptr;
 
 #include <hardware/K32_remote.h>
-K32_remote* remote = nullptr;
+K32_remote *remote = nullptr;
 
 #include <hardware/K32_pwm.h>
-K32_pwm* pwm = nullptr;
+K32_pwm *pwm = nullptr;
+
+#include <hardware/K32_dipswitch.h>
+K32_dipswitch *dipswitch = nullptr;
 
 #include <K32_wifi.h>
-K32_wifi* wifi = nullptr;
+K32_wifi *wifi = nullptr;
 
 #include <K32_bluetooth.h>
-K32_bluetooth* bt = nullptr;
+K32_bluetooth *bt = nullptr;
 
 #include <K32_osc.h>
-K32_osc* osc = nullptr;
+K32_osc *osc = nullptr;
 
 #include <K32_mqtt.h>
-K32_mqtt* mqtt = nullptr;
+K32_mqtt *mqtt = nullptr;
 
 #include <K32_web.h>
-K32_web* web = nullptr;
+K32_web *web = nullptr;
 
 #include <K32_artnet.h>
-K32_artnet* artnet = nullptr;
+K32_artnet *artnet = nullptr;
 
 #include <K32_light.h>
-K32_light* light = nullptr;
+K32_light *light = nullptr;
 
 #include "K32_dmx.h"
-K32_dmx* dmx = nullptr;
+K32_dmx *dmx = nullptr;
 
 #if HW_ENABLE_AUDIO == 1
-    #include <audio/K32_audio.h>
-    K32_audio* audio = nullptr;
+#include <audio/K32_audio.h>
+K32_audio *audio = nullptr;
 #endif
 
 #include "settings.h"
 
-
 // CHECK MCP SWITCH to select Wifi or BT
 //
-bool wifiMode() {
-    if (mcp) return mcp->state(14);
-    else return true;
+bool wifiMode()
+{
+    if (mcp)
+        return mcp->state(14);
+    else
+        return true;
 }
 
 // K32 loader
 //
-void k32_setup() {
+void k32_setup()
+{
 
     //////////////////////////////////////// K32_lib ////////////////////////////////////
     k32 = new K32(NODE32_VER);
-    // Serial.begin(115200, SERIAL_8N1);
-    
-    //////////////////////////////////////// K32 hardware ////////////////////////////////////
-    // STM32
-    #ifdef HW_ENABLE_STM32
-        stm32 = new K32_stm32(k32);
+// Serial.begin(115200, SERIAL_8N1);
 
-        LOGF("[stm32]: API_REV: %d\n",            stm32->api_rev());
-        LOGF("[stm32]: FIRMWARE_REV: %d\n",       stm32->firmware_rev());
-        LOGF("[stm32]: HW_ID: %d\n",              stm32->hw_id());
-        LOGF("[stm32]: HW_REV: %d\n",             stm32->hw_rev());
-    #endif
+//////////////////////////////////////// K32 hardware ////////////////////////////////////
+// STM32
+#ifdef HW_ENABLE_STM32
+    stm32 = new K32_stm32(k32);
+
+    LOGF("[stm32]: API_REV: %d\n", stm32->api_rev());
+    LOGF("[stm32]: FIRMWARE_REV: %d\n", stm32->firmware_rev());
+    LOGF("[stm32]: HW_ID: %d\n", stm32->hw_id());
+    LOGF("[stm32]: HW_REV: %d\n", stm32->hw_rev());
+#endif
 
     LOGINL("[system]: NODE id: ");
     LOG(k32->system->id());
@@ -84,42 +90,42 @@ void k32_setup() {
     LOGINL("[system]: HW rev: ");
     LOG(k32->system->hw());
 
-    // AUDIO 
-    #if HW_ENABLE_AUDIO == 1
-        if (AUDIO_PIN[k32->system->hw()][0] > 0 && SD_PIN[k32->system->hw()][0] > 0)
-            audio = new K32_audio(k32, AUDIO_PIN[k32->system->hw()], SD_PIN[k32->system->hw()]);
-    #endif
+// AUDIO
+#if HW_ENABLE_AUDIO == 1
+    if (AUDIO_PIN[k32->system->hw()][0] > 0 && SD_PIN[k32->system->hw()][0] > 0)
+        audio = new K32_audio(k32, AUDIO_PIN[k32->system->hw()], SD_PIN[k32->system->hw()]);
+#endif
 
     // BUTTONS (GPIO)
     buttons = new K32_buttons(k32);
     if (k32->system->hw() == 13 || k32->system->hw() == 14) // ATOM
         buttons->add(39, "atom");
 
-    // MCP 
-    #if HW_ENABLE_AUDIO == 1
-        LOG("MCP: disabled when audio in use");
-    #else
-        mcp = new K32_mcp(k32); 
-    #endif
-    k32->on("mcp/press", [](Orderz* order){
-        if (order->getData(0)->toInt() == 14 && strcmp(order->getData(1)->toStr(), "long") == 0) k32->system->reset();
-    });
-    k32->on("mcp/release", [](Orderz* order){
-        if (order->getData(0)->toInt() == 14 && strcmp(order->getData(1)->toStr(), "long") == 0) k32->system->reset();
-    });
+// MCP
+#if HW_ENABLE_AUDIO == 1
+    LOG("MCP: disabled when audio in use");
+#else
+    mcp = new K32_mcp(k32);
+#endif
+    k32->on("mcp/press", [](Orderz *order)
+            {
+        if (order->getData(0)->toInt() == 14 && strcmp(order->getData(1)->toStr(), "long") == 0) k32->system->reset(); });
+    k32->on("mcp/release", [](Orderz *order)
+            {
+        if (order->getData(0)->toInt() == 14 && strcmp(order->getData(1)->toStr(), "long") == 0) k32->system->reset(); });
 
     // POWER
-    if (stm32) power = new K32_power(stm32, LIPO, true);
-    //power->setExternalCurrentSensor(HO10_P_SP33, CURRENT_PIN[k32->system->hw()], PWR_FAKE_CURRENT);  // TODO: test sensor
-    if(mcp && mcp->ok && power) power->setMCPcalib(mcp);
+    if (stm32)
+        power = new K32_power(stm32, LIPO, true);
+    // power->setExternalCurrentSensor(HO10_P_SP33, CURRENT_PIN[k32->system->hw()], PWR_FAKE_CURRENT);  // TODO: test sensor
+    if (mcp && mcp->ok && power)
+        power->setMCPcalib(mcp);
 
     // Remote
     remote = new K32_remote(k32, mcp);
 
-    k32->on("stm32/btn-click", [](Orderz* order){
-        remote->stmNext();
-    });
-
+    k32->on("stm32/btn-click", [](Orderz *order)
+            { remote->stmNext(); });
 
     // PWM
     pwm = new K32_pwm(k32);
@@ -127,51 +133,56 @@ void k32_setup() {
     // /////////////////////////////////////////////// LIGHT //////////////////////////////////////
 
     light = new K32_light(k32);
-    
+
+    if (k32->system->hw() == 1)
+    {
     #if HW_ENABLE_AUDIO == 1
         LOG("DMX: disabled when audio in use");
     #else
-        dmx = new K32_dmx(DMX_PIN[k32->system->hw()], DMX_OUT);    
+        // Todo route dmx in or out
+        dmx = new K32_dmx(DMX_PIN[k32->system->hw()], DMX_OUT);
     #endif
-
+    }
+    else
+    {
+        // Todo route dmx in or out
+        dmx = new K32_dmx(DMX_PIN[k32->system->hw()], DMX_OUT);
+    }
 
     // /////////////////////////////////////////////// NAME //////////////////////////////////////
 
     String nodeName = String(L_NAME) + "-" + String(k32->system->lightid()) + "-v" + String(NODE32_VER) + "-c" + String(k32->system->channel());
 
-    LOG("\nNAME:   " + nodeName );
+    LOG("\nNAME:   " + nodeName);
     LOGF("CHANNEL: %d\n\n", k32->system->channel());
     LOGF("LIGHT ID: %d\n", k32->system->lightid());
-    
+
     /////////////////////////////////////////////// NETWORK //////////////////////////////////////
 
     ////////////////// WIFI MODE
-    if (wifiMode()) 
+    if (wifiMode())
     {
         // WIFI
         LOG("NETWORK: wifi");
         wifi = new K32_wifi(k32);
         wifi->setHostname(k32->system->name() + (nodeName != "") ? "-" + nodeName : "");
 
-
         // MQTT
         mqtt = new K32_mqtt(k32, wifi, stm32, light);
-        
 
         // OSC
-        osc = new K32_osc(k32, wifi, stm32);    // TODO: re-enable OSC
-        
+        osc = new K32_osc(k32, wifi, stm32); // TODO: re-enable OSC
 
         // WEB
         web = new K32_web(k32);
 
-        //ARTNET
-        #if ARTNET_ENABLE == 1
-            artnet = new K32_artnet(k32, wifi, nodeName);
-            artnet->start(); 
+// ARTNET
+#if ARTNET_ENABLE == 1
+        artnet = new K32_artnet(k32, wifi, nodeName);
+        artnet->start();
 
-            K32_artnet::onFullDmx([](const uint8_t *data, int length)
-            {
+        K32_artnet::onFullDmx([](const uint8_t *data, int length)
+                              {
                 // LOGF("Artnet frame %d\n", length);
 
                 // Force Auto
@@ -180,33 +191,30 @@ void k32_setup() {
 
                 // FULL NODE
                 if (ARTNET_DMXNODE && dmx) 
-                    dmx->setMultiple(data, length);
-            });
-        #endif
-
-
+                    dmx->setMultiple(data, length); });
+#endif
     }
-    
+
     ////////////////// BLUETOOTH MODE
-    else {
+    else
+    {
         // BLUETOOTH
         LOG("NETWORK: bluetooth");
         LOG("NETWORK: bluetooth DISABLED! too much memory consumed... FIXME");
-        
+
         // bt = new K32_bluetooth(k32, "k32-" + String(k32->system->id()));
 
         // // TODO use event !
         // bt->onConnect([&]() {
         //     bt->send("Yo Rasta!");
         // });
-
     }
-    
+
     ////////////////// RUN SPECIFIC DEVICE
     setup_device();
 
-
     ///////////////// BLINK READY
-    uint8_t l[6] = {255,255,255,255,255,255};
-    if (stm32) stm32->blink(l, 500);
+    uint8_t l[6] = {255, 255, 255, 255, 255, 255};
+    if (stm32)
+        stm32->blink(l, 500);
 }
